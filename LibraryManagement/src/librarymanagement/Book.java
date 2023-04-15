0e31e79adb220e0927324d15a75fbbd58d63ac99
CoalRocks <julijangarbek@gmail.com>
2023-04-15 13:37:20 -0400

    Initial commit

---
+++ LibraryManagement/src/librarymanagement/Book.java
@@ -0,0 +1,5 @@
+package librarymanagement;
+
+public class Book {
+
+}
---
+++ LibraryManagement/src/librarymanagement/FileManager.java
@@ -0,0 +1,76 @@
+package librarymanagement;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class FileManager {
+    private static final String BOOKS_FILE = "books.txt";
+    private static final String USERS_FILE = "users.txt";
+    private static final String COLUMN_SEP = ",";
+
+    public static List<Book> loadBooks() {
+        List<Book> books = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(BOOKS_FILE))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] bookData = line.split(",");
+                Book book = new Book(bookData[0], bookData[1], bookData[2], bookData[3]);
+                books.add(book);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading books file: " + e.getMessage());
+        }
+        return books;
+    }
+
+    public static void saveBooks(Collection<Book> books) {        
+        try (PrintWriter out = new PrintWriter(
+                               new BufferedWriter(
+                               new FileWriter(BOOKS_FILE)))) {
+
+            // write all products to the file
+            for (Book book : books) {
+                out.print(book.getTitle() + COLUMN_SEP);
+                out.print(book.getAuthor() + COLUMN_SEP);
+                out.println(book.getISBN() + COLUMN_SEP);
+                out.println(book.getPublicationDate());
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static List<User> loadUsers() {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(USERS_FILE))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] userData = line.split(",");
+                users.add(new User(userData[0], userData[1]));
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading users file: " + e.getMessage());
+        }
+        return users;
+    }
+
+    public static void saveUsers(Collection<User> users) {
+        try (PrintWriter out = new PrintWriter(
+                               new BufferedWriter(
+                               new FileWriter(USERS_FILE)))) {
+
+            // write all products to the file
+            for (User user : users) {
+                out.print(user.getId() + COLUMN_SEP);
+                out.println(user.getName());
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
+
+
